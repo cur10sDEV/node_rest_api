c8e76974bbf696c415f27694185d256f0ef4ae17
cur10sDEV <specsx@protonmail.com>
2023-07-20 13:26:07 +0530

    added helmet compression morgan

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 require("dotenv").config();
 const connectDB = require("./db/connectDB");
 const multer = require("multer");
 const { initIO } = require("./utils/socket");
+const helmet = require("helmet");
+const compresssion = require("compression");
+const morgan = require("morgan");
 
 connectDB();
 // routes
@@ -13,8 +17,16 @@ const adminRoutes = require("./routes/admin");
 const authRoutes = require("./routes/auth");
 const errorHandler = require("./middlewares/errorHandler");
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "logs", "access.log"),
+  { flags: "a" }
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(compresssion());
+app.use(morgan("combined", { stream: accessLogStream }));
 
 app.use("/images", express.static(path.join(__dirname, "images")));
 
